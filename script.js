30052d0287652677491a3f55c1e45981b652e32b
rikerik <bako.erik.dominik@gmail.com>
2023-05-25 20:56:13 +0200

    Added array to store the todos and a function which adds todos to the array

--- script.js
+++ script.js
@@ -5,7 +5,27 @@
 class Model {
     //Manages the data of an application
     constructor() {
+        //The state of the model, an rray of todo object, prepopulated with some data
+        //empty array of the model
+        this.todos = [
+            { id: 1, text: 'Finish todo app', complete: false },
+            { id: 2, text: 'Sleep 8 hours', complete: false },
+        ]
     }
+
+    //function for adding todos
+    addTodo(todoText) {
+        const todo = {
+            //If the length of this.todos is greater than 0, then the code retrieves the
+            //last element from the array and increments its id value by 1.
+            id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+            text: todoText,
+            complete: false,
+        }
+        //here we add the const todo to the array
+        this.todos.push(todo);
+    }
+
 }
 
 class View {
@@ -16,9 +36,12 @@ class View {
 
 class Controller {
     //links the user and the system
-    //The app will be an instance of the controller.
     constructor(model, view) {
         this.model = model;
         this.view = view;
     }
 }
+
+//The app will be an instance of the controller.
+const app = new Controller(new Model(), new View());
+
